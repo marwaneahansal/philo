9f1bff531a2bac6be23eef0813ab3be54e0bbc5d
Marwane Ahansal <ahansalmarwane@gmail.com>
2023-04-23 00:24:42 +0000

    fix dying philos issue with usleep while checking the required time

--- philo/threads.c
+++ philo/threads.c
@@ -6,7 +6,7 @@
 /*   By: mahansal <mahansal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 02:11:46 by mahansal          #+#    #+#             */
-/*   Updated: 2023/04/19 06:01:47 by mahansal         ###   ########.fr       */
+/*   Updated: 2023/04/23 00:23:36 by mahansal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,15 +28,6 @@ int create_philos(t_data *data)
 	return (0);
 }
 
-int	is_philo_dead(long current_time, t_philo *philo)
-{
-	// printf("current_time: %ld - last: %ld\n", current_time, philo->last_eat_time);
-	// printf("id: %d - timetodie %d - current_time - last: %ld\n", philo->id, philo->data->time_to_die, current_time - philo->last_eat_time );
-	if (current_time - philo->last_eat_time > philo->data->time_to_die)
-		return (1);
-	return (0);
-}
-
 long  get_ms_time()
 {
 	struct timeval time;
@@ -47,7 +38,8 @@ long  get_ms_time()
 
 void sleep_time(long start_time, long end_time)
 {
-	while (get_ms_time() - start_time < end_time);
+	while (get_ms_time() - start_time < end_time)
+		usleep(500);
 }
 
 void  print_state(t_philo *philo, char *state, int philo_id)
@@ -108,8 +100,8 @@ void  *routine(void *arg)
 	while (1)
 	{
 		pthread_mutex_lock(&philo->data->forks[philo->id - 1]);
-		print_state(philo, "has taken a fork", philo->id);
 		pthread_mutex_lock(&philo->data->forks[philo->id % philo->data->philos_nb]);
+		print_state(philo, "has taken a fork", philo->id);
 		print_state(philo, "has taken a fork", philo->id);
 		print_state(philo, "is eating", philo->id);
 		sleep_time(get_ms_time(), philo->data->time_to_eat);
